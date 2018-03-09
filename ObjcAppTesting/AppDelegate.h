a3d4511a371e659bafcfd179d9a39a33b056d6f3
myusuf08 <ucupp08@gmail.com>
2018-03-09 15:12:19 +0700

    Initial Commit

---
+++ ObjcAppTesting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ObjcAppTesting
+//
+//  Created by MMDC on 09/03/18.
+//  Copyright © 2018 Muh.Yusuf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjcAppTesting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjcAppTesting
+//
+//  Created by MMDC on 09/03/18.
+//  Copyright © 2018 Muh.Yusuf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
